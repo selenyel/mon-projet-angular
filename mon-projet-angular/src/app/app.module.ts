e8237eb5840185923bc2f1ac9067a4872ea9d347
Selen YEL <selenyelyazilim@gmail.com>
2023-05-24 10:14:56 +0300

    initial commit
    initial commit

---
+++ mon-projet-angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ mon-projet-angular/src/app/servers/servers.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.sass']
+})
+export class ServersComponent {
+  allowNewServer = false
+  serverCreationStatus = 'No server was created!'
+  serverName = 'write server name..'
+  listOfServers : string[] = []
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
+    console.log('ne')
+    console.count('ne')
+    console.info('infooo')
+    console.error('error')
+    console.warn('warning, selen yel')
+    console.debug('debug')
+    this.listOfServers.push(this.serverName);
+    this.serverName=''
+  }
+
+  onUpdateServerName(event:any){
+    console.log(event.data)
+    //this.serverName = event.data;
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
+
+/*
+<script> 
+var app = angular.module('myApp', []) 
+app.controller("myController", function ($log) { 
+            $log.log('This is log.'); 
+            $log.error('This is error.'); 
+            $log.info('This is info.'); 
+            $log.warn('This is warning.'); 
+            $log.debug('This is debugging.'); 
+        }); 
+</script> 
+*/
