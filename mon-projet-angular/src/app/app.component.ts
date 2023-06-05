3a173b17fa5627f91c728fc8260b3785e35880a6
selenyel <selenyelyazilim@gmail.com>
2023-06-05 10:34:33 +0300

    Adding tabs and buttons (Cleaning the project with current knowledge)
    (No routing, no hrefs or any other methods other than *ngIf)

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  buttons : string[] = ['Server Adder', 'Username Input', 'Show Password with Log']
   title = 'mon-projet-angular';
+  activeTab = 0;
+
+  navbarClicked(value:any){
+    this.activeTab = this.buttons.indexOf(value.target.innerText)
+  }
 }
